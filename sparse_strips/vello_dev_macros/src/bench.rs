de3d82650dcbc023e869f3553d7a165dbcd33054
valadaptive <valadaptive@protonmail.com>
2025-11-22 15:17:09 -0500

    SSE4.2

--- sparse_strips/vello_dev_macros/src/bench.rs
+++ sparse_strips/vello_dev_macros/src/bench.rs
@@ -77,6 +77,13 @@ pub(crate) fn vello_bench_inner(_: TokenStream, item: TokenStream) -> TokenStrea
             }
 
             #[cfg(target_arch = "x86_64")]
+            if let Some(neon) = Level::new().as_sse4_2() {
+                c.bench_function(&get_bench_name(&#input_fn_name_str, "f32_sse4_2"), |b| {
+                    run_float(b, neon);
+                });
+            }
+
+            #[cfg(target_arch = "x86_64")]
             if let Some(neon) = Level::new().as_avx2() {
                 c.bench_function(&get_bench_name(&#input_fn_name_str, "f32_avx2"), |b| {
                     run_float(b, neon);
